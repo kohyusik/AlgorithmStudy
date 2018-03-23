63e93e2a7b311732fa402ee97a36b1e85669df03
Yusik Koh <yusik.koh@mytrum.com>
2018-03-23 16:49:02 +0900

    guava test source

--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -3,7 +3,11 @@
  */
 package test;
 
+import com.google.common.reflect.TypeParameter;
+import com.google.common.reflect.TypeToken;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yusik
@@ -14,12 +18,24 @@ public class TestMain {
 	public static void main(String[] args) {
 
 		ArrayList<Integer> test = new ArrayList<Integer>();
-
+//		ParameterizedTypeReference<CommonResponse<List<T>>> responseType =
+//				ParameterizedTypeReferenceBuilder.fromTypeToken(
+//						new TypeToken<CommonResponse<List<T>>>() {}
+//								.where(new TypeParameter<T>() {}, resultType));
 		test.add(1);
 		test.add(5);
 		test.add(51);
 		test.indexOf(1);
-		System.out.println(test);
+		test(String.class);
+		
+	}
+	
+	public static <T> void test(Class<T> cls) {
+		TypeToken type = new TypeToken<List<T>>(){}
+				.where(new TypeParameter<T>() {
+				
+				}, cls);
+		System.out.println(type.getType());
 	}
 
 }
