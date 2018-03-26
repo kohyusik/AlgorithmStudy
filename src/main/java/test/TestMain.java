37167588eb30abd072946c36727732cb877cc4fb
kohyusik <newtype26@naver.com>
2018-03-26 11:04:02 +0900

    type test

--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -7,7 +7,7 @@ import com.google.common.reflect.TypeParameter;
 import com.google.common.reflect.TypeToken;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 /**
  * @author yusik
@@ -26,15 +26,37 @@ public class TestMain {
 		test.add(5);
 		test.add(51);
 		test.indexOf(1);
-		test(String.class);
+		
+		Map<String, Map<String, Integer>> map;
+		
+		test(String.class, Object.class);
+		test(String.class, Object.class, Object.class);
 		
 	}
 	
-	public static <T> void test(Class<T> cls) {
-		TypeToken type = new TypeToken<List<T>>(){}
-				.where(new TypeParameter<T>() {
-				
-				}, cls);
+	public static <T, S> void test(Class<T> cls, Class<S> cls2) {
+		TypeToken type = new TypeToken<Map<T, S>>(){}
+				.where(new TypeParameter<T>() {}, cls)
+				.where(new TypeParameter<S>() {}, cls2)
+				;
+		System.out.println(type.getType());
+		System.out.println(type.getRawType());
+	}
+	public static <T, Z, K, X> void test(
+			Class<T> cls, Class<Z> cls2, Class<K> cls3) {
+		TypeToken value = new TypeToken<Map<T, Z>>(){}
+				.where(new TypeParameter<T>() {}, cls)
+				.where(new TypeParameter<Z>() {}, cls2)
+				;
+		
+		Class ptype = value.getRawType();
+//		test(String, ptype);
+//
+		TypeToken type = new TypeToken<Map<K, X>>(){}
+				.where(new TypeParameter<K>() {}, cls3)
+				.where(new TypeParameter<X>() {}, ptype)
+				;
+//		System.out.println(value.getType());
 		System.out.println(type.getType());
 	}
 
