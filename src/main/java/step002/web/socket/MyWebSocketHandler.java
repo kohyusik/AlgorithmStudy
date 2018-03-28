4b1f6390875ab4fe57b42c4f55994f661c15a5f2
kohyusik <newtype26@naver.com>
2018-03-28 09:42:18 +0900

    session 관리 in handler

--- src/main/java/step002/web/socket/MyWebSocketHandler.java
+++ src/main/java/step002/web/socket/MyWebSocketHandler.java
@@ -2,7 +2,10 @@ package step002.web.socket;
 
 import org.springframework.web.socket.*;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * @author : kohyusik
@@ -12,44 +15,64 @@ import java.util.List;
  */
 public class MyWebSocketHandler implements WebSocketHandler {
 
+    Logger logger = Logger.getLogger("MyWebSocketHandler");
+
+    Set<WebSocketSession> sessionSet = new HashSet<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-    
-        System.out.println("connected");
+
+        sessionSet.add(session);
+        logger.info("connected : " + session.getId());
+        logger.info("session list : " + sessionSet);
     }
-    
+
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-    
-        System.out.println(message);
+
+        logger.info(message.toString());
         if (message instanceof TextMessage) {
             this.handleMessage(session, (TextMessage) message);
         }
-    
+
     }
 
     public void handleMessage(WebSocketSession session, TextMessage message) throws Exception {
 
-        System.out.println(message.getPayload());
+        String payload = message.getPayload();
+        System.out.println(payload);
 
-        session.sendMessage(message);
+        sessionSet.parallelStream().forEach((broadcastSession) -> {
+                    try {
+                        broadcastSession.sendMessage(message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        for (WebSocketSession broadcastSession : sessionSet) {
+            broadcastSession.sendMessage(message);
+        }
 
     }
-    
-    @Override
-    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-    
-    }
-    
+
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-    
+        logger.warning(session.toString());
+        sessionSet.remove(session);
+        logger.info("session list : " + sessionSet);
+        logger.info("closed : " + session.getId() + " - " + closeStatus.getReason());
     }
-    
+
     @Override
     public boolean supportsPartialMessages() {
-        
+
         return false;
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
     }
 }
--- src/main/webapp/study/websocket/websocket.js
+++ src/main/webapp/study/websocket/websocket.js
@@ -6,14 +6,16 @@
  */
 
 {
-    var ws = new WebSocket('ws://localhost:8080/ws.server');
+    var ws = new WebSocket('ws://192.168.43.66:8080/ws.server');
     ws.onmessage = function(e) {
-        console.log(e);
+        console.log(e.data);
     };
 
-    var colorLabel = document.querySelector('#container p');
-    colorLabel.addEventListener('click', function(e) {
-        console.log(e);
-        ws.send(e.srcElement.innerHTML);
-    });
+    var colorLabelList = document.querySelectorAll('#container p');
+    for (var i = 0, max = colorLabelList.length; i < max; i++) {
+        colorLabelList[i].addEventListener('click', function(e) {
+            // console.log(e);
+            ws.send(e.srcElement.innerHTML);
+        });
+    }
 }
