1c9f42ac72263c4f42f36b6df375b55929296289
kohyusik <newtype26@naver.com>
2018-03-29 12:56:47 +0900

    Embedded Tomcat 설정 4가지

--- src/main/java/boot/BootApplication.java
+++ src/main/java/boot/BootApplication.java
@@ -1,13 +1,18 @@
 package boot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"step002", "boot"})
 public class BootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+//        HashMap<String, Object> properties = new HashMap<>();
+//        properties.put("server.port", 9000);
+//        new SpringApplicationBuilder(BootApplication.class).properties(properties).run(args);
+        
+        new SpringApplicationBuilder(BootApplication.class).run(args);
+//        SpringApplication.run(BootApplication.class, args);
     }
 }
