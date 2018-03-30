62e76b4002bfacaa45f92bc2b5e5573de427c75b
kohyusik <newtype26@naver.com>
2018-03-30 14:10:32 +0900

    uri components sample

--- src/main/java/step002/web/springboot/SampleController.java
+++ src/main/java/step002/web/springboot/SampleController.java
@@ -3,11 +3,20 @@ package step002.web.springboot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    
+    Logger logger = Logger.getLogger("MyWebSocketHandler");
 
     @RequestMapping("/")
     @ResponseBody
@@ -21,5 +30,25 @@ public class SampleController {
         System.out.println("execution Controller!!");
         return "execution!!!!a!";
     }
+    
+    @RequestMapping(value = "/test", method = { RequestMethod.GET })
+    @ResponseBody
+    public Map test(HttpServletRequest request) {
+        
+        logger.fine("************ TEST ************");
+        
+        UriComponentsBuilder uriComponents = UriComponentsBuilder.newInstance();
+        uriComponents.scheme("http").host("localhost").port(8080).path("/login/{test1}/{test2}")
+                .path("/{test}").path("/{type}");
+        
+        uriComponents.queryParam("eeeee", 23222);
+        String uri  = uriComponents.build().toString();
+    
+        Map<String, String> result = new HashMap<>();
+        result.put("uri", uri);
+        result.put("request", request.toString());
+        return result;
+    }
+    
 
 }
