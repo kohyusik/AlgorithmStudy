0e7d98a7d734ec155e4f661391dbcdbcff21fc8b
kohyusik <newtype26@naver.com>
2018-04-08 19:10:34 +0900

    java queue 내장 함수 비교 : peek() , poll()

--- src/main/java/step001/kao/Test03.java
+++ src/main/java/step001/kao/Test03.java
@@ -1,8 +1,29 @@
 package step001.kao;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Test03 {
 
     public static void main(String[] args) {
+
+        Queue<String> queue = new ArrayDeque<>(2);
+
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        System.out.println(queue);
+
+
+
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        System.out.println(queue.poll()); // remove
+        System.out.println(queue);
+        System.out.println(queue.contains("B"));
+
+
 
     }
 }
