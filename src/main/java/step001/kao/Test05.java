b8011db2a73c2f19eab45b356b75e8aca8ffcc3a
kohyusik <newtype26@naver.com>
2018-04-10 01:22:50 +0900

    print board

--- src/main/java/step001/kao/Test05.java
+++ src/main/java/step001/kao/Test05.java
@@ -13,6 +13,10 @@ public class Test05 {
                  "AAABF",
                  "CCBBF"};
         printBoard(createBoard(4, 5, input1));
+        System.out.println();System.out.println();
+
+        String[] input2 = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+        printBoard(createBoard(6, 6, input2));
     }
 
     public static char[][] createBoard(int height, int width, String[] inputTable) {
@@ -32,7 +36,7 @@ public class Test05 {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[board.length - i - 1][j]);
+                System.out.print(board[i][j]);
             }
             System.out.println();
         }
