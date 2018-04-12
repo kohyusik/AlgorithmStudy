19109ea76b5532d3021b5a5728e8fa32f1ec6f2f
kohyusik <newtype26@naver.com>
2018-04-13 01:33:41 +0900

    board play game

--- src/main/java/step001/kao/Test05.java
+++ src/main/java/step001/kao/Test05.java
@@ -1,23 +1,33 @@
 package step001.kao;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test05 {
 
 
     public static void main(String[] args) {
 
-
         String[] input1 =
                 {"CCBDE",
                  "AAADE",
                  "AAABF",
                  "CCBBF"};
-        printBoard(createBoard(4, 5, input1));
-        searchArount(1, 1, createBoard(4, 5, input1));
-        System.out.println();System.out.println();
-
-        String[] input2 = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
-        printBoard(createBoard(6, 6, input2));
+        System.out.println("score : " + playGame(createBoard(4, 5, input1)));
+        System.out.println();System.out.println();System.out.println("==========================");System.out.println();
+        
+        
+        
+        
+        String[] input2 = {
+        		"TTTANT", 
+        		"RRFACC", 
+        		"RRRFCC", 
+        		"TRRRAA", 
+        		"TTMMMF", 
+        		"TMMTTJ"};
+        System.out.println("score : " + playGame(createBoard(6, 6, input2)));
+        System.out.println();System.out.println();System.out.println("==========================");System.out.println();
     }
 
     public static char[][] createBoard(int height, int width, String[] inputTable) {
@@ -35,6 +45,7 @@ public class Test05 {
 
     public static void printBoard(char[][] board) {
 
+        System.out.println();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j]);
@@ -43,22 +54,84 @@ public class Test05 {
         }
     }
 
-    public static void searchArount(int x, int y, char[][] board) {
-
-        if (x > 0 && x < board[0].length) {
-            if (y > 0 && y < board.length) {
-                System.out.println(board[x - 1][y - 1]);
-                System.out.println(board[x - 1][y]);
-                System.out.println(board[x - 1][y + 1]);
-                System.out.println(board[x][y - 1]);
-                System.out.println(board[x][y + 1]);
-                System.out.println(board[x + 1][y - 1]);
-                System.out.println(board[x + 1][y]);
-                System.out.println(board[x + 1][y + 1]);
+    public static int playGame(char[][] board) {
+    	
+    	int score = 0;
+    	
+    	Set<String> set = new HashSet<>();
+    	
+        for (int i = 0; i < board.length - 1; i++) {
+            for (int j = 0; j < board[i].length - 1; j++) {
+            	
+            	char src = board[i][j];
+            	
+            	if (src != ' '
+            			&& src == board[i + 1][j] 
+            			&& src == board[i + 1][j + 1]
+            			&& src == board[i][j + 1]) {
+            		
+            		set.add(new Coordinate(i, j).toString());
+            		set.add(new Coordinate(i + 1, j).toString());
+            		set.add(new Coordinate(i, j + 1).toString());
+            		set.add(new Coordinate(i + 1, j + 1).toString());
+            	}
             }
         }
+        printBoard(board);
+        
+        if (set.size() != 0) {
+        	score += set.size();
+        	removeBlock(board, set);
+        	score += playGame(board);
+        }
+        
+    	return score;
 
+    }
+    
+    public static void removeBlock(char[][] board, Set<String> set) {
+    	
+    	// remove block
+    	for (String coordStr : set) {
+    		String[] coord = coordStr.split("/");
+    		int x = Integer.parseInt(coord[0]);
+    		int y = Integer.parseInt(coord[1]);
+    		board[x][y] = ' ';
+		}
+//    	printBoard(board);
+    	
 
+    	// down block
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+            	if (board[i][j] == ' ') {
+            		for (int k = i - 1; k >= 0; k--) {
+            			if (board[k][j] != ' ') {
+            				board[i][j] = board[k][j];
+            				board[k][j] = ' ';
+//            		    	printBoard(board);
+            			}
+            		}
+            	}
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    public static class Coordinate {
+    	public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+    	int x;
+    	int y;
+    	@Override
+    	public String toString() {
+    		return x + "/" + y;
+    	}
     }
 
 }
