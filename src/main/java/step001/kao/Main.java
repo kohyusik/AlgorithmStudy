2560d5f063b1411e66b531c887eed73c37cba347
kohyusik <newtype26@naver.com>
2018-04-14 00:09:48 +0900

    time search

---
+++ src/main/java/step001/kao/Main.java
@@ -0,0 +1,39 @@
+package step001.kao;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        while (true) {
+            new Main().solve();
+
+        }
+    }
+
+    public void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int general = sc.nextInt();
+        int acting = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            result++;
+            array[i] -= general;
+
+            if (array[i] > 0) {
+                result += Math.ceil(1.0 * array[i] / acting);
+            }
+
+        }
+        System.out.println(result);
+    }
+}
