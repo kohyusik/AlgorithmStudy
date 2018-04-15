a92cd6094861fd1bc3e560eea6fc5476848251b6
kohyusik <newtype26@naver.com>
2018-04-15 15:59:01 +0900

    test init

--- src/main/java/step001/kao/Temp.java
+++ src/main/java/step001/kao/Temp.java
@@ -1,92 +1,46 @@
 package step001.kao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
 public class Temp {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public static void main(String[] args) {
+		System.out.println(decode("23511011501782351112179911801562340161171141148"));
+		System.out.println(decode("51156511"));
+	}
 
+	/*
+	 * Complete the decode function below.
+	 */
+	public static String decode(String encoded) {
+		/*
+		 * Write your code here.
+		 */
 
-        long a = getJaccard("FRANCE", "french");
-        long b = getJaccard("handshake", "shake hands");
-        long c = getJaccard("aa1+aa2", "AAAA12");
-        long d = getJaccard("E=M*C^2", "e=m*c^2");
+		StringBuffer sb = new StringBuffer();
+		char[] input = encoded.toCharArray();
+		char[] output = new char[input.length];
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-    }
+		String output1;
+		for (int i = 0; i < input.length; i++) {
+			output[output.length - i - 1] = input[i];
+		}
 
-// FRANCE  french   16384
-// handshake shake hands  65536
-// aa1+aa2  AAAA12   43690
-// E=M*C^2  e=m*c^2   65536
+		System.out.println(output);
 
-    public static long getJaccard(String str1, String str2){
+		for (int i = 0; i < output.length - 1; i += 2) {
 
-        List<String> listA = new ArrayList<String>();
-        List<String> listB = new ArrayList<String>();
+			int temp = Integer.parseInt(String.valueOf(output[i]) + String.valueOf(output[i + 1]));
 
-        str1 = str1.toUpperCase();
-        for(int i = 0; i< str1.length()-1;i++){
-            if(Pattern.matches("^[A-Z]*$",str1.substring(i,i+2))){
-                listA.add(str1.substring(i,i+2));
-            }
-        }
+			if (temp >= 10 && temp <= 12) {
+				temp = temp * 10 + Integer.parseInt(String.valueOf(output[i + 2]));
+				i += 1;
+			}
+			System.out.println(temp);
 
-        str2 = str2.toUpperCase();
-        for(int i = 0; i< str2.length()-1;i++){
-            if(Pattern.matches("^[A-Z]*$",str2.substring(i,i+2))){
-                listB.add(str2.substring(i,i+2));
-            }
-        }
+			sb.append((char) temp);
 
-        List<String> intersection = new Temp().intersection(listA, listB);
-        List<String> union = new Temp().union(listA, listB);
+		}
 
-        double intersectionSize = intersection.size();
-        double unionSize = union.size();
-        if (intersectionSize == 0){
-            return 1 * 65536;
-        }else{
-            return (long)(intersectionSize/unionSize * 65536);
-        }
+		return sb.toString();
 
-    }
-
-    public <T> List<T> union(List<T> list1, List<T> list2) {
-//        Set<T> set = new HashSet<T>();
-//
-//        set.addAll(list1);
-//        set.addAll(list2);
-        //  return new ArrayList<T>(set);
-        List listA = new ArrayList<T>();
-
-        for(int i = 0; i<list1.size();i++){
-            listA.add(list1.get(i));
-        }
-        for(int i = 0; i<list2.size();i++){
-            listA.add(list2.get(i));
-        }
-        List intersection = intersection(list1, list2);
-        for(int i = 0; i<intersection.size();i++){
-            listA.remove(intersection.get(i));
-        }
-        return listA;
-    }
-
-    public <T> List<T> intersection(List<T> list1, List<T> list2) {
-        List<T> list = new ArrayList<T>();
-
-        for (T t : list1) {
-            if (list2.contains(t)) {
-                list.add(t);
-            }
-        }
-        return list;
-    }
+	}
 }
