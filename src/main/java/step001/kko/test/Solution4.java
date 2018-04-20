a6cccd2dcf855b2cb0505bc591a24618c3027a5f
kohyusik <newtype26@naver.com>
2018-04-20 22:34:40 +0900

    source refactoring

--- src/main/java/step001/kko/test/Solution4.java
+++ src/main/java/step001/kko/test/Solution4.java
@@ -5,39 +5,39 @@ public class Solution4 {
     static int solve(String str) {
         String programmer = "programmer";
 
-    	String test = programmer;
+    	String head = programmer;
 
     	int i = 0;
     	// programmer
         for (; i < str.length(); i++) {
-            int pIndex = test.indexOf(str.charAt(i));
+            int pIndex = head.indexOf(str.charAt(i));
             if (pIndex != -1) {
-                test = test.substring(0, pIndex).concat(test.substring(pIndex + 1));
+                head = head.substring(0, pIndex).concat(head.substring(pIndex + 1));
             }
 
-            if (test.length() == 0) {
+            if (head.length() == 0) {
                 i++;
                 break;
             }
         }
 
-    	String test2 = programmer;
+    	String tail = programmer;
 
     	int j = str.length() - 1;
     	// programmer
         for (; j >= 0; j--) {
-            int pIndex = test2.indexOf(str.charAt(j));
+            int pIndex = tail.indexOf(str.charAt(j));
             if (pIndex != -1) {
-                test2 = test2.substring(0, pIndex).concat(test2.substring(pIndex + 1));
+                tail = tail.substring(0, pIndex).concat(tail.substring(pIndex + 1));
             }
 
-            if (test2.length() == 0) {
+            if (tail.length() == 0) {
                 j--;
                 break;
             }
         }
 
-        System.out.println(test);
+        System.out.println(head);
         System.out.println(i);
         System.out.println(j);
 
