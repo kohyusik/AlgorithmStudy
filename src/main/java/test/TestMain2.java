ed4c07c0f277d987d8eadc34fa7e1df8e523b842
kohyusik <newtype26@naver.com>
2018-05-28 23:20:21 +0900

    get recursive super class

--- src/main/java/test/TestMain2.java
+++ src/main/java/test/TestMain2.java
@@ -3,13 +3,7 @@
  */
 package test;
 
-import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.security.SecureRandom;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author yusik
@@ -17,29 +11,49 @@ import java.util.regex.Pattern;
 public class TestMain2 {
     
     public static void main(String[] args) throws IOException {
+    
+        System.out.println(22);
         
+        Class clazz = Object.class;
+        
+        Class supClazz =clazz.getSuperclass();
+        System.out.println(clazz);
+        System.out.println(clazz == Object.class);
+        System.out.println(supClazz);
+    
+    
+        StringBuffer baseUrl = new StringBuffer("http://localhost:8080/test/test2/test3");
+        baseUrl.delete(baseUrl.indexOf("/test/test2/test3"), baseUrl.length());
+        System.out.println(baseUrl);
+        
+        
+        
+        
+        
+        
+        /*
         String renamedFileName = "ABC_123_15";
-        
+    
         //        File file = File.createTempFile("0javaTest", ".txt");
         File file = new File("C:\\git\\AlgorithmStudy\\src\\main\\webapp\\study\\reactjs\\react004.js");
     
         System.out.println(file.canRead());
         System.out.println(file.canWrite());
         System.out.println(file.canExecute());
-        
+    
         System.out.println(file.toURI());
         System.out.println(file.getName());
         System.out.println(file.getPath());
         System.out.println(file.length());
         SecureRandom sr = new SecureRandom();
         System.out.println(sr.getAlgorithm());
-        
+    
         String EMAIL = "^[a-zA-Z0-9]+@[a-zA-Z0-9]*[.][a-zA-Z]*$";
         String MOBILE = "(01[012679])(d{3,4})(d{4})";
-        
+    
         String EMAIL_PATTERN = "^[a-z0-9A-Z._-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$";
         String PHONE_PATTERN = "(01[016789])(\\d{3,4})(\\d{4})";
-        
+    
         Matcher m = Pattern.compile(EMAIL).matcher("@aa.a");
         System.out.println(m.matches());
         Matcher m2 = Pattern.compile(MOBILE).matcher("0101223450123");
@@ -48,7 +62,7 @@ public class TestMain2 {
         System.out.println(m3.matches());
         Matcher m4 = Pattern.compile(PHONE_PATTERN).matcher("01063757314");
         System.out.println(m4.matches());
-   /*
+   *//*
         try {
             String serverIP = "127.0.0.1"; // 127.0.0.1 & localhost 본인
             System.out.println("서버에 연결중입니다. 서버 IP : " + serverIP);
@@ -73,20 +87,20 @@ public class TestMain2 {
             ie.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
-   
-   
-        
+        }*//*
+    
+    
+    
         try {
             Class clazz = TestMain.class;
             System.out.println(clazz.getDeclaredMethods());
             Method method = clazz.getDeclaredMethod("refTest", String.class);
             Method method2 = clazz.getDeclaredMethod("refTest", Integer.class);
             Object ret = method.invoke(clazz.newInstance(), "TEST22");
-    
+        
             System.out.println(ret);
-            
-            
+        
+        
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -96,7 +110,7 @@ public class TestMain2 {
         } catch (InstantiationException e) {
             e.printStackTrace();
         }
-    
+    */
     }
     
 }
