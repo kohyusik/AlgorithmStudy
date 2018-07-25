08e76cad6b6b6b84b48dcb5d8aa710c6f3d933f4
kohyusik <newtype26@naver.com>
2018-07-25 17:34:56 +0900

    string search: brute force

--- src/main/java/step001/string/search/BruteForce.java
+++ src/main/java/step001/string/search/BruteForce.java
@@ -9,13 +9,35 @@ package step001.string.search;
 public class BruteForce {
     
     public static void main(String[] args) {
-        String total = "ZABBCABCEIBCZ";
-        String search = "ABC";
-    
+        String total = "ZABCCBCABCABCABEIBCZ";
+        String search = "ABCDF";
+        
         System.out.println(searchString(total, search));
+        
     }
     
     public static int searchString(String text, String search) {
-        return text.indexOf(search);
+        char[] source = text.toCharArray();
+        char[] target = search.toCharArray();
+    
+        System.out.println(source[0]);
+        System.out.println((int)source[1]);
+    
+        System.out.println(source[1] == 65);
+        byte[] b = "AaBCD".getBytes();
+        
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+        
+        byte bt = 0xF;
+        System.out.println(bt);
+        System.out.println(0xFF);
+        System.out.println(017);
+        
+        
+        
+        int t = text.indexOf(search);
+        return t;
     }
 }
