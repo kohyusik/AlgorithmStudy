fcdb65d6feed6df5dccf7b6febbf65c72d76f81c
kohyusik <newtype26@naver.com>
2018-07-26 13:42:49 +0900

    string search : brute force 구현

--- src/main/java/step001/string/search/BruteForce.java
+++ src/main/java/step001/string/search/BruteForce.java
@@ -9,35 +9,43 @@ package step001.string.search;
 public class BruteForce {
     
     public static void main(String[] args) {
-        String total = "ZABCCBCABCABCABEIBCZ";
-        String search = "ABCDF";
-        
-        System.out.println(searchString(total, search));
+        String total = "ZABCCBCABCABXABCABDIBCZ"; // 23
+        String search = "ABCABD"; // 6
+    
+        int test = total.indexOf(search);
+        System.out.println(test);
+    
+        int test2 = searchString(total, search);
+        System.out.println(test2);
         
     }
     
     public static int searchString(String text, String search) {
         char[] source = text.toCharArray();
         char[] target = search.toCharArray();
-    
-        System.out.println(source[0]);
-        System.out.println((int)source[1]);
-    
-        System.out.println(source[1] == 65);
-        byte[] b = "AaBCD".getBytes();
+        int posSource = 0;
+        int result = -1;
         
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
+        while (posSource <= source.length - target.length) {
+    
+            int posTarget = 0;
+            if (source[posSource] == target[posTarget]) {
+    
+                for (; posTarget < target.length; posSource++, posTarget++) {
+                    if (source[posSource] != target[posTarget]) {
+                        break;
+                    }
+                }
+                
+                if (posTarget == target.length) {
+                    result = posSource - posTarget;
+                    break;
+                }
+            
+            }
+            posSource++;
         }
         
-        byte bt = 0xF;
-        System.out.println(bt);
-        System.out.println(0xFF);
-        System.out.println(017);
-        
-        
-        
-        int t = text.indexOf(search);
-        return t;
+        return result;
     }
 }
