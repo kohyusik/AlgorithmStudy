b34617d08c8df54bd8889fcb1fecff6099eba109
kohyusik <newtype26@naver.com>
2018-08-21 16:47:26 +0900

    comment 정리

--- src/main/java/step002/web/java8/StreamTest.java
+++ src/main/java/step002/web/java8/StreamTest.java
@@ -1,5 +1,9 @@
 package step002.web.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * @author : kohyusik
  * @version : 1.0
@@ -7,5 +11,24 @@ package step002.web.java8;
  * @description :
  */
 public class StreamTest {
+    
+    public static void main(String[] args) {
+        
+        List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+    
+        System.out.println(myList.hashCode());
+    
+        myList
+                .stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+    
+        System.out.println(myList.hashCode());
+        System.out.println(Objects.hash("kohyusik"));
+        System.out.println("kohyusik".hashCode());
+        
+    }
     
 }
