2666d7b93485f3ae52e6352c414165e08b6a0635
kohyusik <newtype26@naver.com>
2018-09-03 23:14:55 +0900

    lesson2 oddoccurrencesinarray

--- src/main/java/step001/codility/lesson2/OddOccurrencesInArray.java
+++ src/main/java/step001/codility/lesson2/OddOccurrencesInArray.java
@@ -1,5 +1,10 @@
 package step001.codility.lesson2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 /**
  * @author : kohyusik
  * @version : 1.0
@@ -14,7 +19,7 @@ public class OddOccurrencesInArray {
         OddOccurrencesInArray oddOccurrencesInArray = new OddOccurrencesInArray();
     
         // test case 1
-        int[] t1 = new int[]{4, 7, 2, 3};
+        int[] t1 = new int[]{9, 3, 9, 3, 9, 7, 9};
         int solution = oddOccurrencesInArray.solution(t1);
         System.out.println(solution);
         
@@ -22,8 +27,35 @@ public class OddOccurrencesInArray {
     }
     
     public int solution(int[] A) {
+
+        ArrayList<Integer> indexList = new ArrayList<>();
+
+        for (int i = 0; i < A.length - 1; i++) {
+            if (indexList.contains(i)) {
+                continue;
+            }
+            for (int j = i + 1; j < A.length; j++) {
+                if (!indexList.contains(i) && (A[i] == A[j])) {
+                    indexList.add(i);
+                    indexList.add(j);
+                }
+            }
+        }
+
+        List<Integer> result = indexList.stream()
+                .sorted()
+                .collect(toList());
+
+
+        int s = 0;
+        for (int i = 0; i < result.size() - 1; i++) {
+            if (result.get(i + 1) - result.get(i) > 1) {
+                s = i + 1;
+            }
+        }
+        System.out.println(result);
     
-        return 0;
+        return A[s];
     }
     
 }
