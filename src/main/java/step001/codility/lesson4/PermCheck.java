fcba82c9782a50bf1d1ab4cb7adacce250d177e2
kohyusik <newtype26@naver.com>
2018-09-10 14:46:04 +0900

    comment

--- src/main/java/step001/codility/lesson4/PermCheck.java
+++ src/main/java/step001/codility/lesson4/PermCheck.java
@@ -1,4 +1,7 @@
-package step001.codility.lesson5;
+package step001.codility.lesson4;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : kohyusik
@@ -6,64 +9,37 @@ package step001.codility.lesson5;
  * @date : 2018-08-04
  * @description :
  */
-public class PassingCar {
+public class PermCheck {
 
     public static void main(String[] args) {
-        PassingCar passingCar = new PassingCar();
+        PermCheck permCheck = new PermCheck();
 
         // test case 1
-        int[] test1 = new int[] {0, 1, 0, 1, 1};
-        int count = passingCar.solution(test1);
+        int[] test1 = new int[]{4, 1, 2, 3};
+        int count = permCheck.solution(test1);
         System.out.println("Answer : " + count);
-
-        int[] test2 = new int[] {0, 0, 0, 1, 1};
-        int count2 = passingCar.solution(test2);
-        System.out.println("Answer : " + count2);
-
-        int[] test3 = new int[] {0, 1, 1, 0, 1, 0, 1};
-        int count3 = passingCar.solution(test3);
-        System.out.println("Answer : " + count3);
 
     }
 
     public int solution(int[] A) {
-        // 0 1 0 1 1
 
-        // 0 1 2 3 4
-        // 0   0
-        //   1   1 1
-        // 5
 
-        // 0 1 2 3 4
-        // 0 0 0
-        //       1 1
-        // 6
-
-        // 0 1 2 3 4 5 6
-        // 0     0   0
-        //   1 1   1   1
-        // 7
-
-        // 0 1 1 0 1 0 1
-        //
-        int countEast = 0;
-        int result = 0;
+        List<Integer> cache = new ArrayList<>();
 
         for (int i = 0; i < A.length; i++) {
-            if (A[i] == 0) {
-                countEast += 1;
+            if (A[i] > A.length) {
+                return 0;
             } else {
-                result += countEast;
+
+                if (cache.indexOf(A[i]) != -1) {
+                    return 0;
+                }
+                cache.add(A[i]);
             }
 
 
-            if (result > 1000000000) {
-                return -1;
-            }
         }
 
-        return result;
-
+        return 1;
     }
-
 }
